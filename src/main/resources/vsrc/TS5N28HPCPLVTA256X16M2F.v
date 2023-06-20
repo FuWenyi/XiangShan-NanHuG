7c0b198ed4dbe37c24623e2a1b876f4d00ac65fd
bigzyb <735474903@qq.com>
2023-06-20 11:02:31 +0800

    ADD SRAM Verilog

---
+++ src/main/resources/vsrc/TS5N28HPCPLVTA256X16M2F.v
@@ -0,0 +1,23 @@
+module TS5N28HPCPLVTA256X16M4F(
+    Q, CLK, CEB, WEB, A, D
+);
+parameter Bits = 16;
+parameter Word_Depth = 256;
+parameter Add_Width = 8;
+
+output  reg [Bits-1:0]      Q;
+input                   CLK;
+input                   CEB;
+input                   WEB;
+input   [Add_Width-1:0] A;
+input   [Bits-1:0]      D;
+
+reg [Bits-1:0] ram [0:Word_Depth-1];
+always @(posedge CLK) begin
+    if(!CEB && !WEB) begin
+        ram[A] <= D;
+    end
+    Q <= !CEB && WEB ? ram[A] : {4{$random}};
+end
+
+endmodule
