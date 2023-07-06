8b3bc3600fe4afe24fe33d77851d52d48592e551
bigzyb <735474903@qq.com>
2023-07-06 18:31:06 +0800

    ADD AXI4RAM DTCM

--- src/main/scala/device/AXI4DTCM.scala
+++ src/main/scala/device/AXI4DTCM.scala
@@ -4,6 +4,13 @@ import chisel3._
 import chipsalliance.rocketchip.config.Parameters
 import freechips.rocketchip.diplomacy.AddressSet
 import utils._
+import freechips.rocketchip.amba.axi4.AXI4SlaveNode
+import freechips.rocketchip.amba.axi4.AXI4SlavePortParameters
+import freechips.rocketchip.amba.axi4.AXI4SlaveParameters
+import freechips.rocketchip.diplomacy.RegionType
+import freechips.rocketchip.diplomacy.TransferSizes
+import freechips.rocketchip.diplomacy.LazyModuleImp
+import freechips.rocketchip.diplomacy.LazyModule
 
 class AXI4DTCM()(implicit p: Parameters) {
   
@@ -25,4 +32,11 @@ class AXI4DTCM()(implicit p: Parameters) {
     // to use port(for example): in.ar.bits.addr / in.aw.bits.data / in.b.fire() / in.w.valid / in.r.ready / 
     // AXI bus width: beatBytes
   }
+
+  private val sram = LazyModule(new AXI4RAM(
+    address=Seq(AddressSet(0x8,   ~0x808)),//Seq[0x20000, 0x3ff],
+    memByte= 4096,
+    executable = false
+  ))  
+  sram.node := node
 }
